8909f91ef3428d8588caca4c390c5c7ca1faa99f
Delziron Lima <delziron.braz@hotmail.com>
2024-01-24 21:23:28 -0300

    build: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from 'styled-components';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { GlobalStyles } from './stylesheets/global';
+import { dark } from './stylesheets/themes';
+import { useRef } from 'react';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+import { Home } from './pages/Home';
+
+const App = () => {
+
+  const containerRef = useRef(null);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyles />
+      <ThemeProvider theme={dark} >
+        <LocomotiveScrollProvider
+          options={
+            {
+              smooth: true,
+              // ... all available Locomotive Scroll instance options 
+            }
+          }
+          watch={
+            [
+              //..all the dependencies you want to watch to update the scroll.
+              //  Basicaly, you would want to watch page/location changes
+              //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+            ]
+          }
+          containerRef={containerRef}
+        >
+          <main data-scroll-container ref={containerRef}>
+            <Home />
+          </main>
+        </LocomotiveScrollProvider>
+      </ThemeProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
